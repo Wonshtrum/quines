104910166b8c050f2b351f4d280cdfa43ff17047
Eloi DEMOLIS <eloi.demolis@clever-cloud.com>
2026-01-18 12:23:56 +0100

    Optimized version of lang
    Signed-off-by: Eloi DEMOLIS <eloi.demolis@clever-cloud.com>

---
+++ lang/src_lang_opti.c
@@ -0,0 +1,74 @@
+_escaped = 0;
+_width = 80;
+_pad = 22;
+_data = "`%c`S[psc,,@,x1+Xx8.0=[0Xy1+Ypsa,,@,0])]FbDb1.2.9+B1[b?Cc'#=[d?Cx7.9=[c3.4Cf_,f_,C0],,f_,d1+Dd?],![cc'$=c'^=|2*-Cf_,0],,b1+Bb?]0[";
+_post = "/// Wonshtrum ]";
+
+char c,*o,R[2000],O[]="#";
+long printf(),strcpy(),S[512]={0},v[26]={10,(long)R,[15]=(long)printf},a,b,*s;
+
+int main() {
+	o=O;
+	s=S;
+	strcpy(R, O);
+
+	while (c = *o++) {
+		if (c > 47 && c < 58) {
+			*++s = c - 48;
+		} else
+		if (c > 96 && c < 123) {
+			*++s = v[c-97];
+		} else
+		if (c > 64 && c < 91) {
+			v[c-65] = *s--;
+		} else
+		if (c == 39) {
+			*++s = *o++;
+		} else
+		if (c == 46) {
+			*s = *s**v + *o++ - 48;
+		} else
+		if (c == 33)
+			*s = !*s;
+		else
+		if (c == 63) {
+			*s = *(char*)*s;
+		} else
+		if (c == 44) {
+			;s--;
+		} else
+		if (c == 93)
+		       *s-- && (o = O + *s);
+		else
+		if (c == 59) {
+			s -= *s--;
+		} else
+		if (c == 96) {
+			*++s = (long)o;
+			while (*o && *o - 96) o++;
+			*o++ = 0;
+		} else
+		if (c == 64) {
+			;*s = ((long(*)(...))*s)(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8]);
+		} else
+		if (c == 95) {
+			a = *s;
+			*s = o-O;
+			o = O + a;
+		} else
+		if (c == 91) {
+			*++s = o-O;
+			if (!s[-1]) {
+				for (a=1;a;o++) {
+					*o == 91 && a++;
+					*o == 93 && a--;
+				}
+			}
+		} else {
+			b = *s--;
+			a = *s;
+			long r[] = { [37]=b?a%b:0, a&b, 0, 0, 1, a*b, a+b, 0, a-b, 0, b?a/b:0, [60]=a<b, a==b, a>b, [124]=a|b };
+			*s = r[c];
+		}
+	}
+}
